3c345796143e56c03fe79b2e9cc9233e7a72db9c
273* <keita.y310@icloud.com>
2026-07-26 23:20:19 +0900

    feat: add self intro slide

---
+++ 273Do-theme/layouts/self-intro.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+type Props = {
+  image: string;
+};
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="slidev-layout flex flex-col h-full p-12">
+    <slot name="title" />
+    <div class="flex-1 flex gap-8 items-start mt-14">
+      <img :src="image" class="w-65 aspect-square shrink-0" />
+      <div class="flex flex-col">
+        <slot />
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
--- pages/02-self-intro.md
+++ pages/02-self-intro.md
@@ -1,3 +1,19 @@
-<!--自己紹介スライドに追加する必要があれば設定する-->
+---
+layout: self-intro
+image: /imgs/me.png
+---
 
-## こんにちは
+::title::
+
+# こんにちは
+
+::default::
+
+### 273* (ツナサンド)
+
+Develop, Graphic Design, Sensor Analysis, Experimental Creative, Piano
+
+大阪を拠点に活動している Web エンジニアです。  
+趣味でものづくりや表現活動を楽しんでおり、ツール開発、グラフィックデザイン、映像、音楽、インターネット美学などの実験的な表現に興味があります。
+
+`curl https://273do.works`
